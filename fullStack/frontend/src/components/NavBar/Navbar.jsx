dec6ecc59ab88ac577c9947e7c26636500ae46a8
partengo <thomasbarrerafuentes2006@gmail.com>
2026-04-23 14:55:43 -0300

    Modificación del navbar: Se eliminaron los botones extras

--- fullStack/frontend/src/components/NavBar/Navbar.jsx
+++ fullStack/frontend/src/components/NavBar/Navbar.jsx
@@ -6,10 +6,6 @@ export default function Navbar() {
       <div className="navbar__logo">
         <h1>FitPocket</h1>
       </div>
-      <div className="navbar__actions">
-        <button className="btn btn-secondary">Entrar</button>
-        <button className="btn btn-primary">Empezar gratis</button>
-      </div>
     </nav>
   );
 }
