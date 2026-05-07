c7ecc8931d0c27ada0316899be72f3fca7e2ecee
MBJoel07 <beltran.mauroet36@gmail.com>
2026-05-07 14:47:12 -0300

    Actualicé un componente de Login, puede que haya conflicto de merge

--- fullStack/frontend/src/components/componentsLogin/LoginHero.jsx
+++ fullStack/frontend/src/components/componentsLogin/LoginHero.jsx
@@ -1,11 +1,19 @@
 import Navbar from "../NavBar/Navbar";
 import "./LoginHero.css";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginHero() {
+  const navigate = useNavigate();
+
   return (
     <header className="login-hero">
       <Navbar>
-        <button className="btn btn-secondary">Crear cuenta</button>
+        <button 
+          className="btn btn-secondary" 
+          onClick={() => navigate("/register")}
+        >
+          Crear cuenta
+        </button>
       </Navbar>
     </header>
   );
