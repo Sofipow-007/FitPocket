280fb54072e6beb2adb8229ff9906eaec3cdee62
MBJoel07 <beltran.mauroet36@gmail.com>
2026-05-13 18:59:33 -0300

    No está terminada la vista, faltan corregir varias cosas

--- fullStack/frontend/src/components/componentsOnboarding1/OnboardingHero1.jsx
+++ fullStack/frontend/src/components/componentsOnboarding1/OnboardingHero1.jsx
@@ -4,7 +4,10 @@ import "./OnboardingHero1.css";
 export default function OnboardingHero1() {
   return (
     <header className="onboarding-hero1">
-      <Navbar />
+      <div className="navbar-wrapper">
+        <Navbar />
+        <h2 className="step-title">Paso 1 de 4 - Tu cuerpo</h2>
+      </div>
     </header>
   );
 }
