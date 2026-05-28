fb1a33f09ff70b881c871df494639c5e192bab03
MBJoel07 <beltran.mauroet36@gmail.com>
2026-05-28 13:59:43 -0300

    La nueva página de register ahora está conectada a los botones del Home invitado

--- fullStack/frontend/src/App.jsx
+++ fullStack/frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Onboarding1 from "./pages/Onboarding1/Onboarding1";
 import HomeGuest from "./pages/HomeGuest/HomeGuest";
+import Register from "./pages/Register/Register"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<HomeGuest />} />
             <Route path="/onboarding" element={<Onboarding1 />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </main>
       </div>
